8c7ee903a62a3cd1966ef6114b1aafafe535f93a
Andrii Stempel <72231@student.wsiz.edu.pl>
2026-01-12 14:25:15 +0100

    6

---
+++ lab6/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+public class Student
+{
+    public int StudentId { get; set; }
+    public string Imie { get; set; } = "";
+    public string Nazwisko { get; set; } = "";
+    public List<Ocena> Oceny { get; set; } = new();
+}
+public class Ocena
+{
+    public int OcenaId { get; set; }
+    public double Wartosc { get; set; }
+    public string Przedmiot { get; set; } = "";
+    public int StudentId { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string connectionString =
+        "Data Source=10.200.2.28;" + //"(LocalDb)\\MSSQLLocalDB;" - dla lokalnej bazy
+        "Initial Catalog=studenci_72231;" + //USTAW SWÓJ NUMER!
+       "Integrated Security=True;" +
+        "Encrypt=True;" +
+        "TrustServerCertificate=True";
+        try
+        {
+            using SqlConnection connection = new
+            SqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("Połączono z bazą.");
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine("Wystąpił błąd: " + exc);
+        }
+
+    }
+}
